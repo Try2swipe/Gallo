6aefeeb792c36d44ca0919642c809efa1f4d86b5
Try2swipe <60209241+Try2swipe@users.noreply.github.com>
2025-10-05 00:30:10 -0400

    inline-7.js

---
+++ assets/inline-7.js
@@ -0,0 +1,8 @@
+window.GoogleMaps = {"latitude":8.9816859923373,"longitude":-79.510638875854};
+    window.GoogleMaps.isEnable = 1;
+
+    require.config({
+        paths: {
+            omsGoogleCoordinates: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyArSAvyyeOLbjZiaTlesPpMuA2Mb819D58&libraries=places'
+        }
+    });
