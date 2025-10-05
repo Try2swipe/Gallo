0d8a32b4578209af79a8a27ab43c2cdc1520bb52
Try2swipe <60209241+Try2swipe@users.noreply.github.com>
2025-10-05 00:27:41 -0400

    inline-25.js

---
+++ assets/inline-25.js
@@ -0,0 +1,4 @@
+    <label class="{{cssClasses.label}} {{#isRefined}}checked{{/isRefined}}">
+        <input class="{{cssClasses.checkbox}}" {{#isRefined}}checked{{/isRefined}} type="checkbox" value="{{value}}" />
+        {{label}}
+        <span class="{{cssClasses.count}}">{{count}}</span></label>
