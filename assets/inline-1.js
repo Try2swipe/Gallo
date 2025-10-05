dbc7d3741fcd5863984c3a07251a2903d5d3ce68
Try2swipe <60209241+Try2swipe@users.noreply.github.com>
2025-10-05 00:17:18 -0400

    inline-1.js

---
+++ assets/inline-1.js
@@ -0,0 +1,12 @@
+var LOCALE = 'es\u002DCR'; var BASE_URL = 'https\u003A\u002F\u002Fwww.elgallomasgallo.com.gt\u002F'; var require = { 'baseUrl': 'https\u003A\u002F\u002Fwww.elgallomasgallo.com.gt\u002Fstatic\u002Fversion1757340799\u002Ffrontend\u002FOmnipro\u002Fmonge_base_child_elgallo\u002Fes_CR' };
+
+================================================================================
+FILE: /assets/inline-10.js
+================================================================================
+require.config({
+        map: {
+            '*': {
+                wysiwygAdapter: 'mage/adminhtml/wysiwyg/tiny_mce/tinymceAdapter'
+            }
+        }
+    });
